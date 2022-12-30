da9c3a72121255d8e00de5457681072fd042ed13
nilay <nilayshirke52@gmail.com>
2022-12-30 11:32:54 +0530

    learning express routing

--- express/index.js
+++ express/index.js
@@ -1,16 +1,23 @@
 //importing express
 const express = require('express');
+const path = require('path');
 ///creating app for express
+
 const app = express();
+const publicPath = path.join(__dirname,'public');
+app.use(express.static(publicPath));
 
 //making route for homepage
 app.get('',(req,res)=>{
-    res.send('Hello, How is your day so far?');
+    console.log("data sent by browser",req.query.name)
+    res.send(`Hello ${req.query.name}, How is your day so far?`);
 });
+
+console.log(publicPath);
 
 //making route for about page
 app.get('/about',(req,res)=>{
-    res.send('Hello, How this is about you!');
+    res.sendFile(`${publicPath}/about.html`);
 });
 
 //creating server at 4500
